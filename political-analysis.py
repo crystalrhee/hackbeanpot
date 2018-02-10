b02204327e088cd1e14b3a22afe3e43ee961594e
sarahzhang <zhang.sar@husky.neu.edu>
2018-02-10 16:51:52 -0500

    requests py

--- political-analysis.py
+++ political-analysis.py
@@ -8,34 +8,37 @@ import requests
 app = Flask(__name__)
 
 indicoio.config.api_key = '35b0d68ba113813880b422207eed08d3'
-collection = Collection("collection_name")
+collection = Collection("pro_vs_con", domain="standard")
 
 @app.route('/abstract')
 def get_abstract():
     url = request.args.get("url")
 
     abstract_content = _find_abstract(url)
-    sentiment = indicoio.sentiment(sanitize(abstract_content))
 
-    # collection.add_data([["text1", "label1"], ["text2", "label2"], ...])
+    return render_template("index.html", content=abstract_content, output=abstract_content)
 
-    return render_template("index.html", content=abstract_content, output=sentiment)
 
 
 def _find_abstract(url):
     abstract_box = _scrape_page(url, "div", {"class":"article-con"})
-    return abstract_box.find('p').text
+    return abstract_box
 
 
 def _scrape_page(url, name, attributes):
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    return soup.find(name, attrs=attributes)
+    soup = BeautifulSoup(page.text, "lxml")
+    cons = []
+    for att in soup.find_all(name, attributes):
+        p = att.find('p')
+        cons.append(p.text)
+        collection.add_data([p.text, "gun control con"])
+    return cons
 
-#def train():
-   #collection.train()
+def train():
+   collection.train()
    #collection.wait()
-   #return collection.predict("indico is so easy to use!")
+   return collection.predict("gun control sucks")
 
 if __name__ == '__main__':
     app.run(debug=True)
