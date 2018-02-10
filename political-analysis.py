0f0717259b965369bfc8ee84b0f6e47d29a71380
Olivia Wan <olivia.wan@novartis.com>
2018-02-09 23:57:57 -0500

    proof of concept for indico and web scraper

--- political-analysis.py
+++ political-analysis.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import urllib2
+from bs4 import BeautifulSoup
+import indicoio
 
 app = Flask(__name__)
 
+indicoio.config.api_key = '35b0d68ba113813880b422207eed08d3'
 
-@app.route('/')
+@app.route('/analyze')
 def hello_world():
-    return render_template("index.html")
+    url = request.args.get("url")
+
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    img_box = soup.find("figure", attrs={"class": "lead-img"})
+    img = img_box.find("img")
+    img_src = str(img['src']).split("?")[0]
+
+    print(indicoio.fer(img_src))
+
+    return render_template("index.html", url=img_src)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
