eee32efb5e62c76d38a638962a05cce6963dbbc6
sarahzhang <zhang.sar@husky.neu.edu>
2018-02-10 15:06:59 -0500

    requests py

--- political-analysis.py
+++ political-analysis.py
@@ -1,26 +1,41 @@
 from flask import Flask, render_template, request
-import urllib2
 from bs4 import BeautifulSoup
 import indicoio
+from indicoio.custom import Collection
+from htmllaundry import sanitize
+import requests
 
 app = Flask(__name__)
 
 indicoio.config.api_key = '35b0d68ba113813880b422207eed08d3'
+collection = Collection("collection_name")
 
-@app.route('/analyze')
-def hello_world():
+@app.route('/abstract')
+def get_abstract():
     url = request.args.get("url")
 
-    page = urllib2.urlopen(url)
-    soup = BeautifulSoup(page, "html.parser")
-    img_box = soup.find("figure", attrs={"class": "lead-img"})
-    img = img_box.find("img")
-    img_src = str(img['src']).split("?")[0]
+    abstract_content = _find_abstract(url)
+    sentiment = indicoio.sentiment(sanitize(abstract_content))
 
-    print(indicoio.fer(img_src))
+    # collection.add_data([["text1", "label1"], ["text2", "label2"], ...])
 
-    return render_template("index.html", url=img_src)
+    return render_template("index.html", content=abstract_content, output=sentiment)
 
+
+def _find_abstract(url):
+    abstract_box = _scrape_page(url, "div", {"class":"article-con"})
+    return abstract_box.find('p').text
+
+
+def _scrape_page(url, name, attributes):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup.find(name, attrs=attributes)
+
+#def train():
+   #collection.train()
+   #collection.wait()
+   #return collection.predict("indico is so easy to use!")
 
 if __name__ == '__main__':
     app.run(debug=True)
