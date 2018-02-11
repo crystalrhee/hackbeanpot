30fd6f759cfe92199a9496ce842e5bc801641b56
Olivia Wan <olivia.wan@novartis.com>
2018-02-10 19:53:21 -0500

    simplifying ui

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, flash
 from indicoio.custom import Collection
-from wtforms import Form, TextAreaField, validators, StringField
+from wtforms import Form, TextAreaField, validators
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-
-
-class ReusableForm(Form):
-    name = StringField('Name:', validators=[validators.required()])
 
 
 class TextAreaForm(Form):
@@ -22,22 +18,16 @@ indicoio.config.api_key = '35b0d68ba113813880b422207eed08d3'
 collection = Collection("pro_vs_con", domain="standard")
 
 
-@app.route('/collect', methods=["GET", "POST"])
+@app.route('/collect')
 def collect_from_article():
-    form = ReusableForm(request.form)
+    url = request.args.get("url")
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "lxml")
 
-    print form.errors
-    if request.method == 'POST':
-        url = request.form['name']
-        page = requests.get(url)
-        soup = BeautifulSoup(page.text, "lxml")
+    _add_articles_to_collection(soup, "newblue-pro-quote-box", "pro")
+    _add_articles_to_collection(soup, "newblue-con-quote-box", "con")
 
-        _add_articles_to_collection(soup, "newblue-pro-quote-box", "pro")
-        _add_articles_to_collection(soup, "newblue-con-quote-box", "con")
-
-        flash("Articles successfully added")
-
-    return render_template('index.html', form=form, title="hey")
+    return "Articles successfully added"
 
 
 def _add_articles_to_collection(soup, class_name, label):
@@ -53,20 +43,18 @@ def _add_articles_to_collection(soup, class_name, label):
 @app.route('/clear')
 def clear_collection():
     collection.clear()
-    return render_template("index.html", status="Collection successfully cleared!")
+    return "Collection successfully cleared!"
 
 
 @app.route("/train")
 def train():
     collection.train()
     collection.wait()
-    return render_template("index.html", status="Training completed successfully!")
+    return "Training completed successfully!"
 
 
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
-    # text = request.data
-    # return str(collection.predict(text))
     form = TextAreaForm(request.form)
 
     if request.method == 'POST':
@@ -74,7 +62,7 @@ def predict():
         prediction = collection.predict(text)
         flash(str(prediction))
 
-    return render_template('index.html', wordform=form, title="hey")
+    return render_template('index.html', wordform=form, )
 
 
 if __name__ == '__main__':
