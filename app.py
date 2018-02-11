7c11a3b98cbe94d1b7fa779408a31af32eca9bc9
sarahzhang <zhang.sar@husky.neu.edu>
2018-02-10 20:56:15 -0500

    add select menu yay

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, flash
 from indicoio.custom import Collection
-from wtforms import Form, TextAreaField, validators
+from wtforms import Form, TextAreaField, validators, SelectField
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -12,11 +12,17 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 class TextAreaForm(Form):
     name = TextAreaField('Text', validators=[validators.required()])
+    choices = SelectField('Model', choices=[("gun_control", "Gun Control"), ("death_penalty", "Death Penalty"),
+                                            ("climate_change", "Climate Change")])
 
 
 indicoio.config.api_key = '35b0d68ba113813880b422207eed08d3'
-collection = Collection("pro_vs_con", domain="standard")
-
+gc_collection = Collection("gun_control", domain="standard")
+dp_collection = Collection("death_penalty", domain="standard")
+cc_collection = Collection("climate_change", domain="standard")
+questions = {"gun_control": "Should More Gun Control Laws Be Enacted?",
+             "death_penalty": "Should the Death Penalty Be Allowed?",
+             "climate_change": "Is Human Activity Primarily Responsible for Global Climate Change?"}
 
 @app.route('/collect')
 def collect_from_article():
@@ -24,32 +30,47 @@ def collect_from_article():
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "lxml")
 
-    _add_articles_to_collection(soup, "newblue-pro-quote-box", "pro")
-    _add_articles_to_collection(soup, "newblue-con-quote-box", "con")
+    model_name = request.args.get("model_name")
+    _add_articles_to_collection(soup, "newblue-pro-quote-box", "pro", model_name)
+    _add_articles_to_collection(soup, "newblue-con-quote-box", "con", model_name)
 
     return "Articles successfully added"
 
 
-def _add_articles_to_collection(soup, class_name, label):
+def _add_articles_to_collection(soup, class_name, label, model_name):
     articles = []
     for article in soup.find_all("div", {"class": class_name}):
         contents = article.text
         articles.append([contents, label])
-        collection.add_data(articles)
+
+    if model_name == "gun_control":
+        gc_collection.add_data(articles)
+    elif model_name == "death_penalty":
+        dp_collection.add_data(articles)
+    elif model_name == "climate_change":
+        cc_collection.add_data(articles)
 
     return articles
 
 
 @app.route('/clear')
 def clear_collection():
-    collection.clear()
+    gc_collection.clear()
     return "Collection successfully cleared!"
 
 
 @app.route("/train")
 def train():
-    collection.train()
-    collection.wait()
+    model_name = request.args.get("model_name")
+    if model_name == "gun_control":
+        gc_collection.train()
+        gc_collection.wait()
+    elif model_name == "death_penalty":
+        dp_collection.train()
+        dp_collection.wait()
+    elif model_name == "climate_change":
+        cc_collection.train()
+        cc_collection.wait()
     return "Training completed successfully!"
 
 
@@ -59,10 +80,16 @@ def predict():
 
     if request.method == 'POST':
         text = request.form['name']
-        prediction = collection.predict(text)
+        model_name = request.form['choices']
+        if model_name == "gun_control":
+            prediction = gc_collection.predict(text)
+        elif model_name == "death_penalty":
+            prediction = dp_collection.predict(text)
+        elif model_name == "climate_change":
+            prediction = cc_collection.predict(text)
         flash(str(prediction))
 
-    return render_template('index.html', wordform=form, )
+    return render_template('index.html', wordform=form)
 
 
 if __name__ == '__main__':
