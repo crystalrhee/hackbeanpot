06989c7f889000993683464a4ff0201e21fd5902
Olivia Wan <olivia.wan@novartis.com>
2018-02-10 19:24:43 -0500

    basic form for endpoints

--- political-analysis.py
+++ political-analysis.py
@@ -1,25 +1,43 @@
-from flask import Flask, render_template, request, flash
-from bs4 import BeautifulSoup
 import indicoio
-from indicoio.custom import Collection
 import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, flash
+from indicoio.custom import Collection
+from wtforms import Form, TextAreaField, validators, StringField
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+
+class ReusableForm(Form):
+    name = StringField('Name:', validators=[validators.required()])
+
+
+class TextAreaForm(Form):
+    name = TextAreaField('Text', validators=[validators.required()])
+
 
 indicoio.config.api_key = '35b0d68ba113813880b422207eed08d3'
 collection = Collection("pro_vs_con", domain="standard")
 
 
-@app.route('/collect')
+@app.route('/collect', methods=["GET", "POST"])
 def collect_from_article():
-    url = request.args.get("url")
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "lxml")
+    form = ReusableForm(request.form)
 
-    _add_articles_to_collection(soup, "newblue-pro-quote-box", "pro")
-    _add_articles_to_collection(soup, "newblue-con-quote-box", "con")
+    print form.errors
+    if request.method == 'POST':
+        url = request.form['name']
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "lxml")
 
-    return render_template("index.html", status="Articles successfully added!")
+        _add_articles_to_collection(soup, "newblue-pro-quote-box", "pro")
+        _add_articles_to_collection(soup, "newblue-con-quote-box", "con")
+
+        flash("Articles successfully added")
+
+    return render_template('index.html', form=form, title="hey")
 
 
 def _add_articles_to_collection(soup, class_name, label):
@@ -45,10 +63,18 @@ def train():
     return render_template("index.html", status="Training completed successfully!")
 
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
-    text = request.data
-    return str(collection.predict(text))
+    # text = request.data
+    # return str(collection.predict(text))
+    form = TextAreaForm(request.form)
+
+    if request.method == 'POST':
+        text = request.form['name']
+        prediction = collection.predict(text)
+        flash(str(prediction))
+
+    return render_template('index.html', wordform=form, title="hey")
 
 
 if __name__ == '__main__':
